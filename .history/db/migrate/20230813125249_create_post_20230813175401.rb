9082f9e00ba5cb2faddc6fa1a5787b9b88c3defd
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2023-08-13 17:55:57 +0500

    Creating Post migrations
    * PostTable: Creating post migration with image active and acount ref attribute

---
+++ .history/db/migrate/20230813125249_create_post_20230813175401.rb
@@ -0,0 +1,11 @@
+class CreatePost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :image
+      t.boolean :active
+      
+      t.references :account
+      t.timestamps
+    end
+  end
+end
