edeabd3139f9ec27995531743dfe825dba38025f
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2023-08-21 12:48:34 +0500

    feat: Post the images
    * PostsController: Create and display the post
    * PostModel: Upload multiple image

---
+++ .history/app/models/post_20230821122040.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Post < ApplicationRecord
+  default_scope { order created_at: :desc }
+  has_many_attached :images, dependent: :destroy
+
+  belongs_to :account
+
+  before_create :set_active
+
+  has_many :likes,dependent: :destroy
+  has_many :comments,dependent: :destroy
+
+  scope :active, -> { where(active: true) }
+
+  def thumbnail input
+    if input >= 0 && input < self.images.length
+
+      return self.images[input].variant(resize: "300x300").processed
+  end
+
+  private
+
+  def set_active
+    self.active = true
+  end
+end
