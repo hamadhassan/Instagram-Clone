211793c59484fa03fb2b580b875f17970df9cdda
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2023-08-25 11:20:04 +0500

    fix: the CSRF problem
    * ApplicationContoller: fix the csrf problem

---
+++ .history/app/controllers/accounts_controller_20230825101412.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+# This is the controller responsible for managing user accounts.
+class AccountsController < ApplicationController
+  before_action :authenticate_account!
+  before_action :set_account, only: %i[profile index]
+  before_action :find_post, only: %i[like_user_post unlike_user_post]
+
+  def index
+    @comment = Comment.new
+    following_ids = following_account_ids
+    @follower_suggestions = Account.where.not(id: following_ids + [current_account.id]).limit(5)
+
+    private_accounts = private_accounts_request_accepted
+
+    @posts = relevant_posts(private_accounts, following_ids)
+
+    @posts = @posts.order(created_at: :desc)
+  end
+
+  def accept_follow_request
+    following_id = params[:follow_id]
+    following_account = Account.find_by(id: following_id)
+    follower = Follower.find_by(following_id: following_account.id, follower_id: current_account.id)
+    return unless follower
+
+    redirect_to request.referer
+
+    follower.update(accepted: true)
+    redirect_to request.referer
+  end
+
+  def follower_suggestions
+    following_ids = Relationship.where(follower_id: current_account.id).map(&:following_id)
+    @follower_suggestions = Account.where.not(id: following_ids + [current_account.id])
+    current_account_id = current_account.id
+    @private_accounts_request = Account.joins(:followed_accounts)
+                                       .where(followed_accounts: { following_id: current_account_id, accepted: false })
+  end
+
+  def like_user_post
+    @post.likes.create(account_id: current_account.id, liked: true)
+    redirect_to request.referer
+  end
+
+  def unlike_user_post
+    like = @post.likes.find_by(account_id: current_account.id, liked: true)
+    like.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def create_follower(following_id, follower_id, accepted)
+    Follower.create!(following_id:, follower_id:, accepted:)
+  end
+
+  def set_account
+    @account = Account.find_by_username(params[:username])
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def following_account_ids
+    following_ids = Relationship.where(follower_id: current_account.id).pluck(:following_id)
+    following_ids << current_account.id
+  end
+
+  def private_accounts_request_accepted
+    Account.joins(:following_accounts)
+           .where(following_accounts: { follower_id: current_account.id, accepted: true })
+  end
+
+  def relevant_posts(private_accounts, following_ids)
+    current_account_posts = Post.where(account_id: current_account.id)
+    following_account_posts = Post.where(account_id: following_ids, private: false)
+    following_account_private_posts = Post.where(account_id: private_accounts, private: true)
+
+    current_account_posts.or(following_account_posts).or(following_account_private_posts)
+  end
+end
---
+++ .history/app/controllers/profile_controller_20230825103942.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+# This controller handles the management of profile.
+class ProfileController < ApplicationController
+  before_action :authenticate_account!
+  before_action :set_account
+  def index; end
+
+  def show
+    if account_signed_in? && current_account.id == @account.id
+      current_account_posts = Post.where(account_id: current_account.id)
+      @linked_posts=current_account_posts
+      @show_picture=true
+    elsif  @account.private==true
+      if private_accounts_request_accepted.include(@account.id)
+    `  current_account_posts = Post.where(account_id: @account.id)
+      @linked_posts=current_account_posts
+      @show_picture=true`
+      else
+        @show_picture=false
+        return
+      end
+    else
+      current_account_posts = Post.where(account_id: @account.id, private: false)
+      @linked_posts=current_account_posts
+      @show_picture=true
+    end
+  end
+
+  def follow
+    if @account.private?
+      Relationship.create_or_find_by(follower_id: current_account.id, following_id: @account.id, accepted: false)
+    else
+      Relationship.create_or_find_by(follower_id: current_account.id, following_id: @account.id)
+    end
+    redirect_to request.referer
+  end
+
+  def unfollow
+    current_account.followed_accounts.where(follower_id: current_account.id, following_id: @account.id).destroy_all
+  end
+
+  def accept_follow_request
+    relationship = Relationship.find_by(follower_id: params[:id], following_id: current_account.id)
+    if relationship.present?
+      relationship.update(accepted: true)
+      flash[:success] = 'Follow request accepted.'
+    else
+      flash[:error] = 'Unable to find the follow request.'
+    end
+    redirect_to request.referer
+  end
+
+  private
+
+  def set_account
+    @account = Account.find(params[:id])
+  end
+  def private_accounts_request_accepted
+    Account.joins(:following_accounts)
+           .where(following_accounts: { follower_id: current_account.id, accepted: true })
+  end
+end
